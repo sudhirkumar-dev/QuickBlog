89284f728ce49847435bda918bad818cbb201448
sudhirkumar-dev <kalivarapuskumar@gmail.com>
2023-09-03 12:38:36 +0530

    created all posts page

--- QuickBlog-FE/src/App.jsx
+++ QuickBlog-FE/src/App.jsx
@@ -2,13 +2,15 @@
 
 import { Route, Routes } from "react-router";
 import LoginPage from "./components/LoginPage";
+import AllPosts from "./components/AllPosts";
 
 const App = () => {
   return (
     <>
       <main>
         <Routes>
-          <Route path="/login" element={<LoginPage />}/>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<AllPosts />} />
         </Routes>
       </main>
     </>
